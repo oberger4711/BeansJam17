12a8ddf69b5cf0420a6d0d82bdbac4d89cf07231
Ole Berger <ole.k.berger@t-online.de>
2017-05-14 20:49:50 +0200

    Extract template from ld 33.

---
+++ game/game.ts
@@ -0,0 +1,23 @@
+/// <reference path="phaser.d.ts"/>
+/// <reference path="preload/preload.ts"/>
+
+module GameJam {
+	export class MyGame extends Phaser.Game {
+
+		constructor() {
+			super(900, 600, Phaser.CANVAS, 'content', undefined, undefined, false);
+
+			this.state.add("preload", Preload.Preload);
+			// TODO: Add more states.
+			//this.state.add("intro", Intro.Intro);
+
+			this.state.start("preload");
+		}
+
+	}
+
+}
+
+window.onload = () => {
+	var game = new GameJam.MyGame();
+};
---
+++ game/preload/preload.ts
@@ -0,0 +1,35 @@
+/// <reference path="../phaser.d.ts"/>
+
+module GameJam.Preload {
+	export class Preload extends Phaser.State {
+
+		preload() {
+			var text = this.game.add.text(this.game.width / 2, this.game.height / 2, "Loading...", { fill : '#ffffff' });
+			text.anchor.set(0.5, 0.5);
+
+			// TODO: Load stuff.
+			// Images
+			//this.game.load.image('car', 'assets/car.png');
+
+			// Spritesheets
+			//this.game.load.spritesheet('player', 'assets/player.png', 64, 64);
+
+			// Maps
+			//this.game.load.json('lvl0', 'assets/0.json');
+
+			// Sound
+			//this.game.load.audio('music', 'assets/music.mp3');
+			//this.game.load.audio('laser-snd', 'assets/laser.mp3');
+		}
+
+		create() {
+			// Finished loading.
+			//var music = this.game.add.sound('music', undefined, Number.MAX_VALUE);
+			//music.play();
+			// TODO: Switch to next screen.
+			//this.game.state.start("intro", true, false);
+		}
+
+	}
+
+}
