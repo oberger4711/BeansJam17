006a7f76c97c983c614cc53362e39368686fdc05
Ole Berger <ole.k.berger@t-online.de>
2017-05-20 10:48:16 +0200

    Stick to walls when flying against them.

--- game/level/level.ts
+++ game/level/level.ts
@@ -5,6 +5,11 @@ module GameJam.Level {
 	const LEVEL_MAP_LIST : string[] = ['map_test'];
 	const MAX_FLY_VELOCITY : number = 200;
 
+	enum ELevelState {
+		FLYING,
+		STICKING
+	}
+
 	export class Level extends Phaser.State {
 
 		private music : Phaser.Sound;
@@ -13,8 +18,10 @@ module GameJam.Level {
 		private map : Phaser.Tilemap;
 		private layerSpaceship : Phaser.TilemapLayer;
 		private layerObjects : Phaser.TilemapLayer;
-
 		private player : Phaser.Sprite;
+		private flightLine : Phaser.Line;
+
+		private levelState : ELevelState;
 
 		init(index : number) {
 			this.mapName = LEVEL_MAP_LIST[index];
@@ -43,6 +50,8 @@ module GameJam.Level {
 				console.log("Error: Could not find object with name 'Player' in 'Object Layer 1'");
 			}
 
+			this.flightLine = new Phaser.Line();
+
 			//this.music = this.game.add.sound('music', 1, true);
 			//this.music.play();
 		}
@@ -50,19 +59,40 @@ module GameJam.Level {
 		createPlayer(x : number, y : number) : void {
 			this.player = this.game.add.sprite(x, y, 'player');
 			this.player.animations.add('fly', [0, 1], 100, true);
+			this.player.animations.add('stick', [2, 3], 100, true);
 			this.player.animations.play('fly');
 			this.game.physics.arcade.enable(this.player);
 			this.player.body.velocity.x = -MAX_FLY_VELOCITY;
 			this.player.body.velocity.y = 0;
+			this.levelState = ELevelState.FLYING;
 		}
 
 		update() {
-			this.game.physics.arcade.collide(this.player, this.layerSpaceship);
+			this.game.physics.arcade.collide(this.player, this.layerSpaceship, this.onPlayerCollidesWithSpaceShip, null, this);
+			if (this.levelState == ELevelState.STICKING) {
+				// TODO
+			}
 		}
 
+		private onPlayerCollidesWithSpaceShip(player, spaceShipTile) {
+			console.log("Collision detected of player with spaceship tile " + spaceShipTile + ".");
+			this.transitionToState(ELevelState.STICKING);
+		}
+
+		private transitionToState(next : ELevelState) {
+			if (next == ELevelState.STICKING && this.levelState != ELevelState.STICKING) {
+				console.log("Transitioning to level state " + next + ".");
+				this.player.body.velocity.x = 0;
+				this.player.body.velocity.y = 0;
+				this.levelState = ELevelState.STICKING;
+				this.player.animations.play('stick');
+			}
+		}
+
+
 		render() {
-			this.game.debug.body(this.player);
-			this.game.debug.body(this.layerSpaceship);
+			//this.game.debug.body(this.player);
+			//this.game.debug.body(this.layerSpaceship);
 		}
 	}
 }
